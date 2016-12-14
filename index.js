1e6402d76a8d06cb6e1c1f7b8d3a798e8b161c39
Olav van Schie <olavschie@hotmail.com>
2016-12-14 10:53:11 +0100

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const gulpUtil = require('gulp-util');
+const InlineEnviromentVariablesPlugin = require('inline-environment-variables-webpack-plugin');
+
+const envFile = './.env';
+
+let envEntries = require('dotenv').config({ silent: true, path: envFile });
+
+
+if(Elixir) {
+
+	try {
+
+    	const webpack = require('laravel-elixir-webpack-official');
+
+
+		Elixir.webpack.mergeConfig({
+		    plugins: [
+		        new InlineEnviromentVariablesPlugin(envEntries)
+		    ]
+		});
+
+	} 
+	catch (e) {
+
+	    gulpUtil.PluginError({
+			plugin: 'laravel-elixir-env',
+			message: 'Currently only webpack is supported.'
+	    });
+
+	}
+
+} 
+else {
+
+    gulpUtil.PluginError({
+		plugin: 'laravel-elixir-env',
+		message: 'Please include laravel-elixir-env after Elixir.'
+    });
+
+}
